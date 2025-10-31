ead783f25afeee4b282920b083f7f07736aa7626
Takeda0906 <yyytttccc123@yahoo.co.jp>
2025-10-31 15:30:03 +0900

    Add files via upload

--- hk_family_trip_bookmarks_mobile.py
+++ hk_family_trip_bookmarks_mobile.py
@@ -1,8 +1,3 @@
-import streamlit as st
+import webbrowser
 
-# HTMLファイルを読み込む
-with open("hk_family_trip_bookmarks_mobile.html", "r", encoding="utf-8") as f:
-    html_content = f.read()
-
-# StreamlitでHTMLを表示
-st.components.v1.html(html_content, height=800, scrolling=True)
+webbrowser.open("hk_family_trip_bookmarks_mobile.html")
